c8d7dbcaa2496a1b774a2e12a914c6686ec99e94
wjforero <1499698+wjforero@users.noreply.github.com>
2024-09-12 20:27:08 -0500

    Initial commit

---
+++ app/src/main/java/com/example/simpsonsapp0912/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.simpsonsapp0912
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.simpsonsapp0912.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        binding.changeScreen.setOnClickListener {
+            // abrir otra actividad
+            val goToIntent = Intent(this, CharacterActivity::class.java)
+            startActivity(goToIntent)
+        }
+    }
+}
