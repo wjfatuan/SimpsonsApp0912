d4a924346ed7b6da5e626cfab3c32e1de4369f48
wjforero <1499698+wjforero@users.noreply.github.com>
2024-09-17 21:04:02 -0500

    Implicit intent

--- app/src/main/java/com/example/simpsonsapp0912/CharacterActivity.kt
+++ app/src/main/java/com/example/simpsonsapp0912/CharacterActivity.kt
@@ -1,5 +1,7 @@
 package com.example.simpsonsapp0912
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -8,6 +10,9 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.simpsonsapp0912.databinding.ActivityCharacterBinding
 
 class CharacterActivity : AppCompatActivity() {
+
+    var name: String? = "Bart"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityCharacterBinding.inflate(layoutInflater)
@@ -20,9 +25,38 @@ class CharacterActivity : AppCompatActivity() {
         }
 
         val index = intent.getIntExtra("CHARACTERINDEX", -1)
-        val name = intent.getStringExtra("CHARACTERNAME")
+        name = intent.getStringExtra("CHARACTERNAME")
         binding.characterName.text = name
         val id = resources.getIdentifier(name?.lowercase(), "drawable", packageName)
         binding.characterImage.setImageResource(id)
+
+        binding.btnDoSomething.setOnClickListener {
+            callIntent()
+        }
     }
+
+    fun callIntent() {
+        val intent = Intent(Intent.ACTION_VIEW).apply {
+            data = Uri.parse("https://es.wikipedia.org/w/index.php?search=$name")
+        }
+        startActivity(intent)
+    }
+
+    /**
+     * URL: Uniform Resource Locator (Una URL es una URI)
+     * ejemplos:
+     * http://www.google.com
+     *
+     * URI: Uniform Resource Identifier
+     * schema:id
+     * ejemplos:
+     * tel:5555555
+     * geo:48.8580852,2.2925508
+     * email:thom@gmail.com
+     * http://servidor/ruta
+     */
+
+
+
+
 }
